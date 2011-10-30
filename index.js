11f89221e8b775f139ded5e0a8bd3db59ee41467
Daniel Shaw <github@dshaw.com>
2011-10-30 00:37:38 -0700

    Function release v0.1. Expose Broker/createBroker at the module level.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-module.exports = require('./lib/zero.js');
+
+/*!
+ * socket.io-zero
+ * Copyright(c) 2011 Daniel D. Shaw <dshaw@dshaw.com>
+ * MIT Licensed
+ */
+
+/**
+ * ZeroStore
+ */
+
+exports = module.exports = require('./lib/zero.js');
+
+/**
+ * Version
+ */
+
+exports.version = '0.1.0';
+
+/**
+ * ZeroStore Broker
+ */
+
+exports.Broker = require('./lib/broker.js');
+
+/**
+ * ZeroStore create Broker
+ */
+
+exports.createBroker = exports.Broker.createBroker;

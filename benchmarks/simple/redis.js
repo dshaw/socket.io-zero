d3ceee810e36e2aa14570c8627db0834b5bd5087
Daniel Shaw <github@dshaw.com>
2011-11-21 20:17:18 -0800

    Sidestepping node run socket.io-client. Getting weird disconnects. Added web-based socket.io client for testing.

--- benchmarks/simple/redis.js
+++ benchmarks/simple/redis.js
@@ -2,7 +2,10 @@
  * Module dependencies.
  */
 
-var sio = require('socket.io')
+
+var fs = require('fs')
+  , http = require('http')
+  , sio = require('socket.io')
   , RedisStore = sio.RedisStore
   , payload = require('./payload.json')
 
@@ -12,38 +15,42 @@ var sio = require('socket.io')
 
 var port = parseInt(process.argv[2]) || 8124
   , nodeId = parseInt(process.argv[3]) || 0
-  , io = sio.listen(port)
+  , html = fs.readFileSync(__dirname + '/client.html')
+  , server = http.createServer(function(req, res) {
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.end(html);
+    })
+  , io = sio.listen(server)
+
+server.listen(port)
 
 /**
  * Socket.io server
  */
 
-io.on('listening', function () {
+server.on('listening', function () {
   console.log('NodeId %d listening on :%d', nodeId, port);
   io.sockets.emit('socket.io node ' + nodeId + 'ready');
 });
 
 io.configure(function () {
   io.set('store', new RedisStore({ nodeId: function () { return nodeId } }));
+  io.set('log level', false);
 });
 
-if (nodeId == 1) {
-  io.sockets.once('connection', function (socket) {
-    var runInterval = setInterval(function () {
-      socket.emit('payload', payload)
-    }, 250) // as fast as you can
-
-    console.log(runInterval)
-    clearInterval(runInterval)
-    
-    setTimeout(function () {
-      clearInterval(runInterval)
-      socket.emit('complete')
-    }, 1000) // 1 minutes
-  });
-}
 
 io.sockets.on('connection', function (socket) {
+  if (nodeId == 1) {
+    var runInterval = setInterval(function () {
+      io.sockets.emit('payload', payload)
+    }, 0) // as fast as you can
+
+    setTimeout(function () {
+      clearInterval(runInterval)
+      io.sockets.emit('complete')
+    }, 60*1000) // 1 minutes
+  }
+
   socket.on('results', function (data) {
     console.log(data)
   });
--- benchmarks/simple/zero.js
+++ benchmarks/simple/zero.js
@@ -2,7 +2,9 @@
  * Module dependencies.
  */
 
-var sio = require('socket.io')
+var fs = require('fs')
+  , http = require('http')
+  , sio = require('socket.io')
   , ZeroStore = require('../..')
   , payload = require('./payload.json')
 
@@ -12,43 +14,38 @@ var sio = require('socket.io')
 
 var port = parseInt(process.argv[2]) || 8124
   , nodeId = parseInt(process.argv[3]) || 0
-  , io = sio.listen(port)
+  , html = fs.readFileSync(__dirname + '/client.html')
+  , server = http.createServer(function(req, res) {
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.end(html);
+    })
+  , io = sio.listen(server)
+
+server.listen(port)
 
 /**
  * Socket.io server
  */
 
-io.on('listening', function () {
+server.on('listening', function () {
   console.log('NodeId %d listening on :%d', nodeId, port);
   io.sockets.emit('socket.io node ' + nodeId + 'ready');
 });
 
 io.configure(function () {
   io.set('store', new ZeroStore({ nodeId: nodeId }));
+  io.set('log level', false);
 });
-
-//if (nodeId == 1) {
-//  io.sockets.once('connection', function (socket) {
-//    var runInterval = setInterval(function () {
-//      socket.emit('payload', payload)
-//    }, 250) // as fast as you can
-//
-//    setTimeout(function () {
-//      clearInterval(runInterval)
-//      socket.emit('complete')
-//    }, 1000) // 1 minutes
-//  });
-//}
 
 io.sockets.on('connection', function (socket) {
   if (nodeId == 1) {
     var runInterval = setInterval(function () {
-      socket.emit('payload', payload)
-    }, 250) // as fast as you can
+      io.sockets.emit('payload', payload)
+    }, 0) // as fast as you can
 
     setTimeout(function () {
       clearInterval(runInterval)
-      socket.emit('complete')
+      io.sockets.emit('complete')
     }, 60*1000) // 1 minutes
   }
 
